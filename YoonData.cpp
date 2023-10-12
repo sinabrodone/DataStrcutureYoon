b7bb59cec7d82db17556976f56cdaa8655bbc41f
sinabrodone <bmdok0719@gmail.com>
2023-10-12 20:35:37 +0900

    231012 1강 자료구조 알고리즘의 이해 - 이진탐색, 순차탐색 구현까지

--- YoonData.cpp
+++ YoonData.cpp
@@ -15,6 +15,31 @@ int LSearch(int arr[], int size, int number)
     return -1;
 }
 
+// 이진 탐색
+int BSearch(int ar[], int len, int target)
+{
+    int first = 0;
+    int last = len - 1;
+    int mid;
+
+    while (first <= last)
+    {
+        mid = (first + last) / 2;
+
+        if (target == ar[mid])
+            return mid;
+        else
+        {
+            if (ar[mid] > target)
+                last = mid - 1;
+            else
+                first = mid +1;
+        }
+    }
+
+    return -1;
+}
+
 int main()
 {
     //자료구조 공부 시작.
@@ -22,13 +47,13 @@ int main()
     int idx = 0;
 
     // 순차탐색
-    idx = LSearch(arr, sizeof(arr) / sizeof(int), 4);
+    idx = BSearch(arr, sizeof(arr) / sizeof(int), 4);
     if (idx == -1)
         cout << "탐색 실패" << endl;
     else
         cout << "타겟 저장 인덱스: " << idx << ' ' << endl;
 
-    idx = LSearch(arr, sizeof(arr) / sizeof(int), 7);
+    idx = BSearch(arr, sizeof(arr) / sizeof(int), 7);
     if (idx == -1)
         cout << "탐색 실패" << endl;
     else
