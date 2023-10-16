790458c73ba533ecd5fbfffb4ce0ee4ab7d5289c
sinabrodone <bmdok0719@gmail.com>
2023-10-16 18:49:30 +0900

    231016 재귀

--- YoonData.cpp
+++ YoonData.cpp
@@ -4,70 +4,21 @@
 #include <iostream>
 using namespace std;    
 
-int LSearch(int arr[], int size, int number)
+void HanoiTower(int n, char from, char by, char to)
 {
-    for (int i = 0; i < size; i++)
-    {
-        if (arr[i] == number)
-            return i;
-    }
-
-    return -1;
-}
-
-// 이진 탐색
-int BSearch(int ar[], int len, int target)
-{
-    int first = 0;
-    int last = len - 1;
-    int mid;
-
-    while (first <= last)
-    {
-        mid = (first + last) / 2;
-
-        if (target == ar[mid])
-            return mid;
-        else
-        {
-            if (ar[mid] > target)
-                last = mid - 1;
-            else
-                first = mid +1;
-        }
-    }
-
-    return -1;
+	if (n == 1)
+		printf("원반1 %c에서 %c로 이동\n", from, to);
+	else
+	{
+		HanoiTower(n - 1, from, to, by);
+		printf("원반%d %c에서 %c로 이동\n", n, from, to);
+		HanoiTower(n - 1, by, from, to);
+	}
 }
 
 int main()
 {
-    //자료구조 공부 시작.
-    int arr[] = { 3,5,2,4,9 };
-    int idx = 0;
+	HanoiTower(3, 'A', 'B', 'C');
 
-    // 순차탐색
-    idx = BSearch(arr, sizeof(arr) / sizeof(int), 4);
-    if (idx == -1)
-        cout << "탐색 실패" << endl;
-    else
-        cout << "타겟 저장 인덱스: " << idx << ' ' << endl;
-
-    idx = BSearch(arr, sizeof(arr) / sizeof(int), 7);
-    if (idx == -1)
-        cout << "탐색 실패" << endl;
-    else
-        cout << "타겟 저장 인덱스: " << idx << ' ' << endl;
-
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
