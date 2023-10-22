e42a6d2d061f642f767aac6993e14be8d1276dd9
sinabrodone <bmdok0719@gmail.com>
2023-10-22 18:06:16 +0900

    231022 배열리스트

--- YoonData.cpp
+++ YoonData.cpp
@@ -1,39 +1,8 @@
-// YoonData.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-using namespace std;    
-class Point
-{
-public:
-
-	int xpos;
-	int ypos;
-
-public:
-	Point(int x = 0, int y = 0)
-		:xpos(x), ypos(y)
-	{
-
-	}
-};
-
-template<typename T>
-void Print(T a)
-{
-	cout << a << endl;
-}
-
-template<>
-void Print(Point& inPoint) 
-{
-	cout << inPoint.xpos << ", " << inPoint.ypos;
-}
+#include<stdio.h> 
+#include "ArrayList.h"
 
 
 int main()
 {
-	Point p(2, 3);
-	Print(p);
-	return 0;
+
 }
